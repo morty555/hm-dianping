d3dc3768ab6521817ba830d32b5be72aa5b58b53
morty555 <1650550685@qq.com>
2025-05-08 10:54:39 +0800

    SMS Login

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.hmdp.controller;
 
 
-import cn.hutool.core.bean.BeanUtil;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.dto.UserDTO;
@@ -11,6 +10,7 @@ import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -22,6 +22,7 @@ import javax.servlet.http.HttpSession;
  * </p>
  *
  * @author 虎哥
+ * @since 2021-12-22
  */
 @Slf4j
 @RestController
@@ -40,7 +41,8 @@ public class UserController {
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
         // 发送短信验证码并保存验证码
-        return userService.sendCode(phone, session);
+
+        return userService.sendCode(phone,session);
     }
 
     /**
@@ -50,7 +52,7 @@ public class UserController {
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
         // 实现登录功能
-        return userService.login(loginForm, session);
+        return userService.login(loginForm,session);
     }
 
     /**
@@ -65,7 +67,7 @@ public class UserController {
 
     @GetMapping("/me")
     public Result me(){
-        // 获取当前登录的用户并返回
+        //  获取当前登录的用户并返回
         UserDTO user = UserHolder.getUser();
         return Result.ok(user);
     }
@@ -82,27 +84,5 @@ public class UserController {
         info.setUpdateTime(null);
         // 返回
         return Result.ok(info);
-    }
-
-    @GetMapping("/{id}")
-    public Result queryUserById(@PathVariable("id") Long userId){
-        // 查询详情
-        User user = userService.getById(userId);
-        if (user == null) {
-            return Result.ok();
-        }
-        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
-        // 返回
-        return Result.ok(userDTO);
-    }
-
-    @PostMapping("/sign")
-    public Result sign(){
-        return userService.sign();
-    }
-
-    @GetMapping("/sign/count")
-    public Result signCount(){
-        return userService.signCount();
     }
 }
--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,22 +1,50 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
+    private StringRedisTemplate  stringRedisTemplate;
+
+    public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 1.判断是否需要拦截（ThreadLocal中是否有用户）
-        if (UserHolder.getUser() == null) {
-            // 没有，需要拦截，设置状态码
+        String token = request.getHeader("authorization");
+        if (StrUtil.isBlank(token)) {
+           response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+           return false;
+        }
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(RedisConstants.LOGIN_USER_KEY + token);
+        if(userMap.isEmpty()) {
             response.setStatus(401);
-            // 拦截
             return false;
         }
-        // 有用户，则放行
+
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+        UserHolder.saveUser(userDTO);
+
+        stringRedisTemplate.expire(RedisConstants.LOGIN_USER_KEY+token,RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
+
         return true;
     }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+
 }
